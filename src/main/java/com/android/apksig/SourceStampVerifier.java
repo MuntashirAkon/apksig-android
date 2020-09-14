7515f8919ef6517b2886ad22a74b3dbd72e142e5
Michael Groover <mpgroover@google.com>
2020-09-14 13:21:23 -0700

    Obtain the V1 signing certificate during stamp verification
    Bug: 169094510
    Test: gradlew test
    Change-Id: I17bb9362101bf07192ebc9ae684a5deb9ee257db
    Merged-In: I17bb9362101bf07192ebc9ae684a5deb9ee257db

--- src/main/java/com/android/apksig/SourceStampVerifier.java
+++ src/main/java/com/android/apksig/SourceStampVerifier.java
@@ -16,12 +16,12 @@
 
 package com.android.apksig;
 
-import static com.android.apksig.apk.ApkUtilsLite.computeSha256DigestBytes;
 import static com.android.apksig.Constants.VERSION_APK_SIGNATURE_SCHEME_V2;
 import static com.android.apksig.Constants.VERSION_APK_SIGNATURE_SCHEME_V3;
 import static com.android.apksig.Constants.VERSION_JAR_SIGNATURE_SCHEME;
-import static com.android.apksig.internal.apk.v1.V1SchemeConstants.MANIFEST_ENTRY_NAME;
+import static com.android.apksig.apk.ApkUtilsLite.computeSha256DigestBytes;
 import static com.android.apksig.internal.apk.stamp.SourceStampConstants.SOURCE_STAMP_CERTIFICATE_HASH_ZIP_ENTRY_NAME;
+import static com.android.apksig.internal.apk.v1.V1SchemeConstants.MANIFEST_ENTRY_NAME;
 
 import com.android.apksig.apk.ApkFormatException;
 import com.android.apksig.apk.ApkUtilsLite;
@@ -54,6 +54,7 @@ import java.io.RandomAccessFile;
 import java.nio.BufferUnderflowException;
 import java.nio.ByteBuffer;
 import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
 import java.security.cert.CertificateException;
 import java.security.cert.CertificateFactory;
 import java.security.cert.X509Certificate;
@@ -251,7 +252,7 @@ public class SourceStampVerifier {
             if (mMinSdkVersion < AndroidSdkVersion.N
                     || signatureSchemeApkContentDigests.isEmpty()) {
                 Map<ContentDigestAlgorithm, byte[]> apkContentDigests =
-                        getApkContentDigestFromV1SigningScheme(cdRecords, apk, zipSections);
+                        getApkContentDigestFromV1SigningScheme(cdRecords, apk, zipSections, result);
                 signatureSchemeApkContentDigests.put(VERSION_JAR_SIGNATURE_SCHEME,
                         apkContentDigests);
             }
@@ -296,12 +297,12 @@ public class SourceStampVerifier {
         try {
             signers = ApkSigningBlockUtilsLite.getLengthPrefixedSlice(apkSignatureSchemeBlock);
         } catch (ApkFormatException e) {
-            result.addVerificationError(isV2Block ? ApkVerificationIssue.V2_SIG_MALFORMED_SIGNERS
+            result.addVerificationWarning(isV2Block ? ApkVerificationIssue.V2_SIG_MALFORMED_SIGNERS
                     : ApkVerificationIssue.V3_SIG_MALFORMED_SIGNERS);
             return;
         }
         if (!signers.hasRemaining()) {
-            result.addVerificationError(isV2Block ? ApkVerificationIssue.V2_SIG_NO_SIGNERS
+            result.addVerificationWarning(isV2Block ? ApkVerificationIssue.V2_SIG_NO_SIGNERS
                     : ApkVerificationIssue.V3_SIG_NO_SIGNERS);
             return;
         }
@@ -328,7 +329,7 @@ public class SourceStampVerifier {
                         apkContentDigests,
                         signerInfo);
             } catch (ApkFormatException | BufferUnderflowException e) {
-                signerInfo.addVerificationError(
+                signerInfo.addVerificationWarning(
                         isV2Block ? ApkVerificationIssue.V2_SIG_MALFORMED_SIGNER
                                 : ApkVerificationIssue.V3_SIG_MALFORMED_SIGNER);
                 return;
@@ -379,7 +380,7 @@ public class SourceStampVerifier {
                 }
                 apkContentDigests.put(signatureAlgorithm.getContentDigestAlgorithm(), digestBytes);
             } catch (ApkFormatException | BufferUnderflowException e) {
-                signerInfo.addVerificationError(
+                signerInfo.addVerificationWarning(
                         isV2Signer ? ApkVerificationIssue.V2_SIG_MALFORMED_DIGEST
                                 : ApkVerificationIssue.V3_SIG_MALFORMED_DIGEST);
                 return;
@@ -394,7 +395,7 @@ public class SourceStampVerifier {
                 certificate = (X509Certificate) certFactory.generateCertificate(
                         new ByteArrayInputStream(encodedCert));
             } catch (CertificateException e) {
-                signerInfo.addVerificationError(
+                signerInfo.addVerificationWarning(
                         isV2Signer ? ApkVerificationIssue.V2_SIG_MALFORMED_CERTIFICATE
                                 : ApkVerificationIssue.V3_SIG_MALFORMED_CERTIFICATE);
                 return;
@@ -408,24 +409,45 @@ public class SourceStampVerifier {
         }
 
         if (signerInfo.getSigningCertificate() == null) {
-            signerInfo.addVerificationError(isV2Signer ? ApkVerificationIssue.V2_SIG_NO_CERTIFICATES
-                    : ApkVerificationIssue.V3_SIG_NO_CERTIFICATES);
+            signerInfo.addVerificationWarning(
+                    isV2Signer ? ApkVerificationIssue.V2_SIG_NO_CERTIFICATES
+                            : ApkVerificationIssue.V3_SIG_NO_CERTIFICATES);
             return;
         }
     }
 
+    /**
+     * Returns a mapping of the {@link ContentDigestAlgorithm} to the {@code byte[]} digest of the
+     * V1 / jar signing META-INF/MANIFEST.MF; if this file is not found then an empty {@code Map} is
+     * returned.
+     *
+     * <p>If any errors are encountered while parsing the V1 signers the provided {@code result}
+     * will be updated to include a warning, but the source stamp verification can still proceed.
+     */
     private static Map<ContentDigestAlgorithm, byte[]> getApkContentDigestFromV1SigningScheme(
             List<CentralDirectoryRecord> cdRecords,
             DataSource apk,
-            ZipSections zipSections)
+            ZipSections zipSections,
+            Result result)
             throws IOException, ApkFormatException {
         CentralDirectoryRecord manifestCdRecord = null;
+        List<CentralDirectoryRecord> signatureBlockRecords = new ArrayList<>(1);
         Map<ContentDigestAlgorithm, byte[]> v1ContentDigest = new EnumMap<>(
                 ContentDigestAlgorithm.class);
         for (CentralDirectoryRecord cdRecord : cdRecords) {
-            if (MANIFEST_ENTRY_NAME.equals(cdRecord.getName())) {
+            String cdRecordName = cdRecord.getName();
+            if (cdRecordName == null) {
+                continue;
+            }
+            if (manifestCdRecord == null && MANIFEST_ENTRY_NAME.equals(cdRecordName)) {
                 manifestCdRecord = cdRecord;
-                break;
+                continue;
+            }
+            if (cdRecordName.startsWith("META-INF/")
+                    && (cdRecordName.endsWith(".RSA")
+                        || cdRecordName.endsWith(".DSA")
+                        || cdRecordName.endsWith(".EC"))) {
+                signatureBlockRecords.add(cdRecord);
             }
         }
         if (manifestCdRecord == null) {
@@ -433,6 +455,36 @@ public class SourceStampVerifier {
             // digest is enough since this would affect the final digest signed by the stamp, and
             // thus an empty digest will invalidate that signature.
             return v1ContentDigest;
+        }
+        if (signatureBlockRecords.isEmpty()) {
+            result.addVerificationWarning(ApkVerificationIssue.JAR_SIG_NO_SIGNATURES);
+        } else {
+            for (CentralDirectoryRecord signatureBlockRecord : signatureBlockRecords) {
+                try {
+                    CertificateFactory certFactory = CertificateFactory.getInstance("X.509");
+                    byte[] signatureBlockBytes = LocalFileRecord.getUncompressedData(apk,
+                            signatureBlockRecord, zipSections.getZipCentralDirectoryOffset());
+                    for (Certificate certificate : certFactory.generateCertificates(
+                            new ByteArrayInputStream(signatureBlockBytes))) {
+                        // If multiple certificates are found within the signature block only the
+                        // first is used as the signer of this block.
+                        if (certificate instanceof X509Certificate) {
+                            Result.SignerInfo signerInfo = new Result.SignerInfo();
+                            signerInfo.setSigningCertificate((X509Certificate) certificate);
+                            result.addV1Signer(signerInfo);
+                            break;
+                        }
+                    }
+                } catch (CertificateException e) {
+                    // Log a warning for the parsing exception but still proceed with the stamp
+                    // verification.
+                    result.addVerificationWarning(ApkVerificationIssue.JAR_SIG_PARSE_EXCEPTION,
+                            signatureBlockRecord.getName(), e);
+                    break;
+                } catch (ZipFormatException e) {
+                    throw new ApkFormatException("Failed to read APK", e);
+                }
+            }
         }
         try {
             byte[] manifestBytes =
@@ -451,18 +503,28 @@ public class SourceStampVerifier {
      * verified if {@link #isVerified()} returns true.
      */
     public static class Result {
+        private final List<SignerInfo> mV1SchemeSigners = new ArrayList<>();
         private final List<SignerInfo> mV2SchemeSigners = new ArrayList<>();
         private final List<SignerInfo> mV3SchemeSigners = new ArrayList<>();
-        private final List<List<SignerInfo>> mAllSchemeSigners = Arrays.asList(mV2SchemeSigners,
-                mV3SchemeSigners);
+        private final List<List<SignerInfo>> mAllSchemeSigners = Arrays.asList(mV1SchemeSigners,
+                mV2SchemeSigners, mV3SchemeSigners);
         private SourceStampInfo mSourceStampInfo;
 
         private final List<ApkVerificationIssue> mErrors = new ArrayList<>();
+        private final List<ApkVerificationIssue> mWarnings = new ArrayList<>();
 
         private boolean mVerified;
 
         void addVerificationError(int errorId, Object... params) {
             mErrors.add(new ApkVerificationIssue(errorId, params));
+        }
+
+        void addVerificationWarning(int warningId, Object... params) {
+            mWarnings.add(new ApkVerificationIssue(warningId, params));
+        }
+
+        private void addV1Signer(SignerInfo signerInfo) {
+            mV1SchemeSigners.add(signerInfo);
         }
 
         private void addV2Signer(SignerInfo signerInfo) {
@@ -493,6 +555,14 @@ public class SourceStampVerifier {
                             "Unknown ApkSigResult Signing Block Scheme Id "
                                     + source.signatureSchemeVersion);
             }
+        }
+
+        /**
+         * Returns a {@code List} of {@link SignerInfo} objects representing the V1 signers of the
+         * provided APK.
+         */
+        public List<SignerInfo> getV1SchemeSigners() {
+            return mV1SchemeSigners;
         }
 
         /**
@@ -552,6 +622,13 @@ public class SourceStampVerifier {
         }
 
         /**
+         * Returns the warnings encountered while verifying the APK's source stamp.
+         */
+        public List<ApkVerificationIssue> getWarnings() {
+            return mWarnings;
+        }
+
+        /**
          * Returns all errors for this result, including any errors from signature scheme signers
          * and the source stamp.
          */
@@ -571,19 +648,43 @@ public class SourceStampVerifier {
         }
 
         /**
+         * Returns all warnings for this result, including any warnings from signature scheme
+         * signers and the source stamp.
+         */
+        public List<ApkVerificationIssue> getAllWarnings() {
+            List<ApkVerificationIssue> warnings = new ArrayList<>();
+            warnings.addAll(mWarnings);
+
+            for (List<SignerInfo> signers : mAllSchemeSigners) {
+                for (SignerInfo signer : signers) {
+                    warnings.addAll(signer.getWarnings());
+                }
+            }
+            if (mSourceStampInfo != null) {
+                warnings.addAll(mSourceStampInfo.getWarnings());
+            }
+            return warnings;
+        }
+
+        /**
          * Contains information about an APK's signer and any errors encountered while parsing the
          * corresponding signature block.
          */
         public static class SignerInfo {
             private X509Certificate mSigningCertificate;
             private final List<ApkVerificationIssue> mErrors = new ArrayList<>();
+            private final List<ApkVerificationIssue> mWarnings = new ArrayList<>();
 
             void setSigningCertificate(X509Certificate signingCertificate) {
                 mSigningCertificate = signingCertificate;
             }
 
-            void addVerificationError(int error, Object... params) {
-                mErrors.add(new ApkVerificationIssue(error, params));
+            void addVerificationError(int errorId, Object... params) {
+                mErrors.add(new ApkVerificationIssue(errorId, params));
+            }
+
+            void addVerificationWarning(int warningId, Object... params) {
+                mWarnings.add(new ApkVerificationIssue(warningId, params));
             }
 
             /**
@@ -602,11 +703,19 @@ public class SourceStampVerifier {
             }
 
             /**
+             * Returns a {@link List} of {@link ApkVerificationIssue} objects representing warnings
+             * encountered during processing of this signer's signature block.
+             */
+            public List<ApkVerificationIssue> getWarnings() {
+                return mWarnings;
+            }
+
+            /**
              * Returns {@code true} if any errors were encountered while parsing this signer's
              * signature block.
              */
             public boolean containsErrors() {
-                return mErrors.isEmpty();
+                return !mErrors.isEmpty();
             }
         }
 
